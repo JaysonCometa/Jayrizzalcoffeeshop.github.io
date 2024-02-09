f2f978c93a78954a17dfac61df507b90cec0c874
JaysonCometa <159505385+JaysonCometa@users.noreply.github.com>
2024-02-09 12:23:03 +0800

    Add files via upload

---
+++ IT221 - Project Website Coffee Shop/ITCoffeeshop.js
@@ -0,0 +1,6 @@
+/* console.log("Our Javascript is connected to our website code!!!"); */
+
+var coffee = "Welcome to Jay Rizz Al Coffee Shop Customer Review Page!!!"
+var shop = 2;
+
+alert(coffee);
